661edac6a382203da02a39cc8b106bd3fc363243
lukasburg <lukiburg@web.de>
2022-12-04 19:07:25 +0100

    Refactor day 3-2 to be functional Fix accidental fragment in input for day 4

--- day3/puzzle2.py
+++ day3/puzzle2.py
@@ -1,10 +1,27 @@
 with open("input.txt") as file:
-    input = file.readlines()
+    inp = file.readlines()
 
 
+print(
+    sum(
+        [
+            range(58)[
+                ord(
+                    set(elf1.strip()).intersection(set(elf2.strip())).intersection(set(elf3.strip()))
+                    .pop()
+                )-96]
+            for elf1, elf2, elf3
+            in [(inp[i], inp[i + 1], inp[i + 2])
+                for i in range(0, len(inp), 3)]
+        ]
+    )
+)
+
+
+# zum (etwas) besseren verständnis hier nochmal imperativ statt funktional
 total_value = 0
 offset = 96
-for elf1, elf2, elf3 in [(input[i], input[i+1], input[i+2]) for i in range(0, len(input), 3)]:
+for elf1, elf2, elf3 in [(inp[i], inp[i + 1], inp[i + 2]) for i in range(0, len(inp), 3)]:
     set1, set2, set3 = set(elf1.strip()), set(elf2.strip()), set(elf3.strip())
     badge = set1.intersection(set2).intersection(set3)
     total_value += range(58)[ord(badge.pop())-offset]
