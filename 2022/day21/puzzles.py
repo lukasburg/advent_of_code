e9f957675e695ed0821532afcd42bae5ce35f6a6
LTab <lukiburg@web.de>
2022-12-31 16:46:00 +0100

    Add 21 part 2

--- 2022/day21/puzzles.py
+++ 2022/day21/puzzles.py
@@ -1,10 +1,13 @@
 import re
 
+from sympy import symbols, Eq, solve
+
 with open("input") as file:
     inp = file.readlines()
 
 number_monkey_parser = re.compile('(\w+): (\d+)')
 operation_monkey_parser = re.compile('(\w+): (\w+) ([-*+/]) (\w+)')
+PART_TWO = True
 
 
 class Monkey:
@@ -16,26 +19,26 @@ class Monkey:
     def value(self):
         return None
 
+    def __repr__(self):
+        return f'{self.name}: {self.__class__.__name__}'
+
     @classmethod
-    def parse_monkey(cls, line, humans_for_part_two=False):
+    def parse_monkey(cls, line):
         nr_match = number_monkey_parser.match(line)
         if number_monkey_parser.match(line):
             name, nr = nr_match.groups()
-            if name == 'humn' and humans_for_part_two:
-                return Human(name, None)
             return NumberMonkey(name, int(nr))
         else:
             name, first, operation, second = operation_monkey_parser.match(line).groups()
-            if name == 'root':
-                return RootMonkey(name, operation, first, second)
-            else:
-                return OperationMonkey(name, operation, first, second)
+            return OperationMonkey(name, operation, first, second)
 
 
 class NumberMonkey(Monkey):
     def __init__(self, name, value):
         super().__init__(name)
         self._value = value
+        if PART_TWO and self.name == 'humn':
+            self._value = symbols('humn')
 
     @property
     def value(self):
@@ -57,7 +60,14 @@ class OperationMonkey(Monkey):
 
     @property
     def value(self):
-        return self._calculate_own_value()
+        if PART_TWO and self.name == 'root':
+            first = self.first_operand.value
+            second = self.second_operand.value
+            expr = Eq(first, second)
+            print(expr)
+            return solve(expr, 'humn')
+        self._calculate_own_value()
+        return self._value
 
     def _calculate_own_value(self):
         if self.operation == '*':
@@ -65,97 +75,15 @@ class OperationMonkey(Monkey):
         elif self.operation == '+':
             self._value = self.first_operand.value + self.second_operand.value
         elif self.operation == '/':
-            self._value = self.first_operand.value // self.second_operand.value
+            self._value = self.first_operand.value / self.second_operand.value
         elif self.operation == '-':
             self._value = self.first_operand.value - self.second_operand.value
-
-    def solve_for(self, should_be_value):
-        first, second = None, None
-        try:
-            first = self.first_operand.value
-        except PunyHumanException:
-            pass
-        try:
-            second = self.first_operand.value
-        except PunyHumanException:
-            pass
-        if self.operation == '+':
-            if first:  # known + ??? = should ->  ??? = should - known
-                second_should_have_value = should_be_value - first
-                return self.second_operand.solve_for(second_should_have_value)
-            elif second:
-                first_should_have_value = should_be_value - second
-                return self.first_operand.solve_for(first_should_have_value)
-            else:  # ??? + ??? = should
-                first = self.first_operand.solve_for(should_be_value)
-                second = self.second_operand.solve_for(should_be_value)
-                first_fraction = first // (first + second)
-                second_fraction = second // (first + second)
-                assert self.first_operand.solve_for(first_fraction) == self.second_operand.solve_for(second_fraction)
-                return self.first_operand.solve_for(first_fraction)
-        if self.operation == '*':
-            if first:  # known * ??? = should ->  ??? = should / known
-                second_should_have_value = should_be_value // first
-                return self.second_operand.solve_for(second_should_have_value)
-            elif second:
-                first_should_have_value = should_be_value // second
-                return self.first_operand.solve_for(first_should_have_value)
-            else:  # ??? - ??? = should
-                first = self.first_operand.solve_for(should_be_value)
-                second = self.second_operand.solve_for(should_be_value)
-
-        if self.operation == '-':
-            if first:  # first - ??? = should ->  ??? = first - should
-                second_should_have_value = first - should_be_value
-                return self.second_operand.solve_for(second_should_have_value)
-            elif second:  # ??? - second = should ->  ??? = should + second
-                first_should_have_value = should_be_value + second
-                return self.first_operand.solve_for(first_should_have_value)
-        if self.operation == '/':
-            if first:  # first / ??? = should ->  ??? = first / should
-                second_should_have_value = first // should_be_value
-                return self.second_operand.solve_for(second_should_have_value)
-            elif second:  # ??? / second = should ->  ??? = should * second
-                first_should_have_value = should_be_value * second
-                return self.first_operand.solve_for(first_should_have_value)
-
-
-# Part 2
-class RootMonkey(OperationMonkey):
-    def solution_part_two(self):
-        first_value = None
-        second_value = None
-        try:
-            first_value = self.first_operand.value
-        except PunyHumanException:
-            print('First value unknown')
-        try:
-            second_value = self.second_operand.value
-        except PunyHumanException:
-            print('First value unknown')
-        print(first_value, second_value)
-        known = first_value if first_value else second_value
-        unknown = self.first_operand if not first_value else self.second_operand
-        return unknown.solve_for(known)
-
-
-class PunyHumanException(Exception):
-    pass
-
-
-class Human(NumberMonkey):
-    @property
-    def value(self):
-        raise PunyHumanException('This human is still figuring out, which value to yell')
-
-    def should_be_value(self, value):
-        return value
 
 
 monkey_dict = dict()
 operation_monkeys_need_update = set()
 for line in inp:
-    monkey = Monkey.parse_monkey(line, humans_for_part_two=True)
+    monkey = Monkey.parse_monkey(line)
     if isinstance(monkey, OperationMonkey):
         operation_monkeys_need_update.add(monkey)
     monkey_dict[monkey.name] = monkey
@@ -165,4 +93,8 @@ for operation_monkey in operation_monkeys_need_update:
     operation_monkey.update_operands_to_instances_from_name(monkey_dict)
 
 
-print(monkey_dict['root'].solution_part_two())
+# print(monkey_dict['root'].solution_part_two())
+
+
+print(monkey_dict['root'].value)
+# print(monkey_dict['root'].solution_part_two())
