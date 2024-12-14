2c301db371ce73419e771412667a6f4bb7ab9e5f
lukasburg <lukiburg@web.de>
2024-12-14 20:23:24 +0100

    Add day 14 solution part 1 and 2 with images

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -9,6 +9,8 @@ def parse(string):
     return Garden([[cell for cell in line] for line in string.split("\n")])
 
 class Coord:
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -16,8 +18,13 @@ class Coord:
     def __repr__(self):
         return f"({self.x}|{self.y})"
 
+    def __add__(self, other):
+        if isinstance(other, Coord):
+            return Coord(self.x + other.x, self.y + other.y)
+        return Coord(self.x + other[0], self.y + other[1])
+
     def neighbors(self):
-        return {Coord(self.x, self.y-1), Coord(self.x, self.y+1), Coord(self.x-1, self.y), Coord(self.x+1, self.y)}
+        return {Coord(self.x + x, self.y + y) for x, y in self.directions}
 
     def is_inbounds(self, garden: "Garden"):
         return 0 <= self.x < garden.x_max and 0 <= self.y < garden.y_max
@@ -27,6 +34,19 @@ class Coord:
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+
+class ClockwiseIterator:
+    def __init__(self):
+        self.i = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """Returns direction for line and outwards direction"""
+        self.i = (self.i + 1) % 4
+        return Coord.directions[self.i], Coord.directions[self.i-1]
+
 
 class Garden:
     def __init__(self, garden):
@@ -87,6 +107,26 @@ def calc_perimeter(area: set[Coord]):
         perimeter += len(not_in_same_area)
     return perimeter
 
+
+# for part 2
+def find_any_bottom_right_corner(area: set[Coord]):
+    for c in area:
+        if c + (1, 0) not in area and c + (0, 1) not in area:
+            return c
+
+def calc_perimeter_with_lines(area: set[Coord], garden):
+    for c in area:
+        garden[c] = " "
+    current_corner = find_any_bottom_right_corner(area)
+    garden[current_corner] = "C"
+    been_on = set()
+    direction_iterator = ClockwiseIterator()
+    direction, outwards = next(direction_iterator)
+    # garden[start_corner + outwards] = "#"
+    # garden[start_corner + direction] = "Q"
+
+    print(render(garden))
+
 def fence_price(area: set[Coord]):
     return len(area) * calc_perimeter(area)
 
@@ -95,6 +135,15 @@ def run(file="input.txt"):
     # print(render(garden))
     return sum([fence_price(area) for _, area in find_areas(garden)])
 
+def run2(file="input.txt"):
+    garden = parse(read(file))
+    # print(render(garden))
+    for _, area in find_areas(garden):
+        calc_perimeter_with_lines(area, garden)
+        break
+    # return sum([fence_price(area) for _, area in ])
+
+
 if __name__ == "__main__":
-    print(run("example.txt"))
-    print(run())
+    print(run2("example.txt"))
+    # print(run())
